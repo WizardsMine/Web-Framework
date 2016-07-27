733bdeba401c085e86a003bb97c4e51f1b91fa70
sander758 <sander.elderman@home.nl>
2016-07-27 16:03:59 +0200

    Changes index.php and gitignore

--- index.php
+++ index.php
@@ -2,16 +2,25 @@
 
 $loader = require __DIR__ . '/vendor/autoload.php';
 
-\Wizard\Kernel\App::$Root = __DIR__;
+\Wizard\Kernel\App::$root = __DIR__;
 
 $uri = substr($_SERVER['REQUEST_URI'], strpos($_SERVER['PHP_SELF'], '/index.php'));
 $method = $_SERVER['REQUEST_METHOD'];
-$App = new \Wizard\Kernel\App($uri, $method);
 
-$App->make(new \Wizard\Http\HttpKernel());
+try {
+    $App = new \Wizard\Kernel\App($uri, $method);
 
-$App->send();
+    $App->prepare();
 
-$App->terminate();
+    $App->start();
+
+    $App->send();
+
+} catch (\Wizard\Exception\WizardRuntimeException $e) {
+    $e->showErrorPage();
+} catch (\Throwable $e) {
+    \Wizard\Exception\WizardRuntimeException::showStaticErrorPage($e);
+}
+
 
 
